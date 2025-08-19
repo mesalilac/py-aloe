95b3bbd27382c4d6c9f7a24219a28d107aef3f64
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-08-20 00:55:22 +0300

    feat: add `inspect_text` helper function

--- parser.py
+++ parser.py
@@ -2,6 +2,25 @@ from dataclasses import dataclass
 from tokens import TokenType, Token
 
 
+def inspect_text(text: str, message: str, target: tuple[int, int], source="text"):
+    target_line = target[0]
+    target_column = target[1]
+    line_index = target_line - 1
+
+    lines = text.splitlines()
+    empty_space = " " * (target_column - 1)
+
+    try:
+        line = lines[line_index]
+
+        print(f"--> {source}:{target_line}:{target_column}")
+        print(line)
+        print(f"{empty_space}^ {message}")
+        print()
+    except IndexError:
+        pass
+
+
 @dataclass
 class Parser:
     tokens: list[Token]
