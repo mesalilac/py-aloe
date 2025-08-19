37e5dbf1e78afb06f8ba49bd82733d7c3ad409f2
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-08-19 21:44:51 +0300

    feat: print line and char

--- lexer.py
+++ lexer.py
@@ -8,12 +8,27 @@ class State:
     line = 1
     column = 1
 
+    def get_position(self) -> tuple[int, int]:
+        return (self.line, self.column)
+
 
 @dataclass
 class Lexer:
     text: str
 
     def tokenize(self) -> list[Token]:
+        state = State()
         tokens: list[Token] = []
+
+        for line in self.text.splitlines():
+            state.column = 1
+
+            print(f"line{state.line, state.column}:", line)
+
+            for char in line:
+                print(f"char{state.line, state.column}:", char)
+                state.column += 1
+
+            state.line += 1
 
         return tokens
