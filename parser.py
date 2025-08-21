6f6e1271efdd3072d09b5b8dffdcb90b2f80e48a
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-08-21 21:13:31 +0300

    feat: custom parser error

--- parser.py
+++ parser.py
@@ -36,6 +36,57 @@ def inspect_text(text: str, message: str, target: tuple[int, int], source="text"
         print_line(line_index + 2, lines[line_index + 1])
 
 
+class ParserSyntaxError(Exception):
+    def __init__(self, text: str, message: str, position: tuple[int, int]):
+        self.message: str = message
+        self.position: tuple[int, int] = position
+        self.source: str = "text"
+        self.line: str = ""
+        self.line_before: str | None = None
+        self.line_after: str | None = None
+
+        lines = text.splitlines()
+        line_num, col_num = self.position
+        line_index = line_num - 1
+        if not (0 < line_index < len(lines)):
+            return
+        if line_index > 0:
+            self.line_before = lines[line_index - 1]
+        self.line = lines[line_index]
+        if line_index + 1 < len(lines):
+            self.line_after = lines[line_index + 1]
+
+    def __str__(self):
+        return f"ParserSyntaxError(message: `{self.message}`, position: `{self.position}`,  source: `{self.source}`, line: `{self.line}`, line_before: `{self.line_before}`, line_after: `{self.line_after}`)"
+
+    def print(self):
+        line_num, col_num = self.position
+        line_index = line_num - 1
+
+        prefix_space = " " * len(str(line_num))
+        empty_space = " " * max(col_num - 1, 0)
+
+        def print_line(num: int, content: str | None = None):
+            if content is not None:
+                print(f" {num} | {content}")
+            else:
+                print(f" {prefix_space} |")
+
+        print(f" {prefix_space} --> {self.source}:{line_num}:{col_num}")
+
+        if self.line_before:
+            print_line(line_index, self.line_before)
+
+        print_line(line_num, self.line)
+
+        print(f" {prefix_space} | {empty_space}^")
+        print(f" {prefix_space} | {empty_space}| {self.message}")
+        print_line(line_num)
+
+        if self.line_after:
+            print_line(line_index + 2, self.line_after)
+
+
 @dataclass
 class Parser:
     tokens: list[Token]
@@ -58,19 +109,20 @@ class Parser:
                 value_token = peek(2)
 
                 if not equals_token or equals_token.type != TokenType.EQUALS:
-                    inspect_text(
+                    raise ParserSyntaxError(
                         text=self.text,
                         message="Missing '=' after Key",
-                        target=equals_token.pos,
+                        position=equals_token.pos,
                     )
+
                     index += 1
                     continue
 
                 if not value_token or value_token.type != TokenType.VALUE:
-                    inspect_text(
+                    raise ParserSyntaxError(
                         text=self.text,
                         message="Missing value after '='",
-                        target=value_token.pos,
+                        position=value_token.pos,
                     )
                     index += 1
                     continue
