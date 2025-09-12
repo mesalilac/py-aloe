ac60e2d50711f394919d8fdffe80850a1b25bf54
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-09-12 05:13:54 +0300

    docs: add usage example for package

--- src/py_cfg/__init__.py
+++ src/py_cfg/__init__.py
@@ -1,1 +1,18 @@
+"""Usage:
+```python
+
+from py_cfg import Cfg
+
+cfg = Cfg.from_file("...")
+
+default_username = cfg.get("default_username")
+
+print(default_username)
+
+cfg.set("default_username", "admin")
+
+cfg.save()
+```
+"""
+
 from .cfg import Cfg
